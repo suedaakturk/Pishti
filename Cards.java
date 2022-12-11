43a0f37688c43baacc07b6bda9b0f8595c99a866
Sueda Akturk <aakturk.sueda@gmail.com>
2022-12-11 20:11:08 +0300

    Making process

---
+++ Cards.java
@@ -0,0 +1,11 @@
+public class Cards{
+private String suit;
+private String rank;
+public Cards(String a,String b){
+suit=a;
+rank=b;
+}public void setSuit(String a){suit=a;}
+public void setRank(String a){rank=a;}
+public String getSuit(){return suit;}
+public String getRank(){return rank;}
+}
--- Project.java
+++ Project.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
 public class Project{
-public static void main(String[] args){
-	System.out.println("basardik");
-}
+ public static void createCards(Cards[] deck){
+        Scanner sc= new Scanner(System.in);
+
+        String[] suits={"♠","♣","♥","♦"};
+        String[] ranks={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+      
+		Cards[] temp= new Cards[52];
+        System.out.println("Cards are dealing...");
+
+        for(int i=0; i< deck.length;i++){
+            deck[i]= new Cards(suits[i/13], ranks[i%13]);
+        }
+		for (int i=0; i<deck.length;i++) {
+                System.out.println(deck[i].getSuit()+deck[i].getRank());
+
+        }System.out.println("--------");
+				
+ 
+ 
+        for (int i=0; i<deck.length;i++) {
+            int index = (int) (Math.random() * deck.length);
+            temp[i] = deck[i];
+            deck[i] = deck[index];
+            deck[index] = temp[i];
+		}
+		
+		
+		for (int i=0; i<deck.length;i++) {
+                System.out.println(deck[i].getSuit()+deck[i].getRank());
+				
+		}
+        System.out.println("--------");
+        Cards[] temp1= new Cards[deck.length];
+        for (int i=0; i<deck.length/2;i++) {
+            temp1[i]= deck[26+i];
+            deck[26+i] = deck[i];
+            deck[i] = temp1[i];
+            }
+        for (int i=0; i<deck.length;i++){
+            System.out.println(deck[i].getSuit()+ deck[i].getRank());
+
+        }
+
+        System.out.println("-------");
+
+    }
+
+    public static void main(String[] args){
+        System.out.println("Weolcome to Game Pisti :)...");
+		Cards[] deck= new Cards[52];
+        createCards(deck);
+
+
+    }
 }
