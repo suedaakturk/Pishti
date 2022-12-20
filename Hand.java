b4746f56a679ca37b1b426ab429589e7e43b3609
Sueda Akturk <aakturk.sueda@gmail.com>
2022-12-20 19:36:13 +0300

    Hand class added
    adding new classes to my project

---
+++ Hand.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Hand {
+    Scanner sc = new Scanner(System.in);
+    Random rd = new Random();
+
+    private int chosen;
+    private String name;
+    private int mycurrentNum = 0;
+    private Card[] hand = new Card[4];
+
+
+    private int numCards;
+
+    public Hand(String a) {
+        name = a;
+    }
+
+    public void addCard(Card mycard) {
+        if (mycurrentNum == 4) {
+            System.out.println("4 kart var.");
+        }
+        if (mycurrentNum < 4) {
+            hand[mycurrentNum] = mycard;
+            mycurrentNum++;
+        }
+    }
+
+    public Card returnCardToBoard() {
+        System.out.println("Which card you want to play?");
+        chosen = sc.nextInt();
+        return hand[chosen - 1];
+    }
+
+    public void removeCardFromHand() {
+
+        Card[] temp = new Card[mycurrentNum - 1];
+        for (int i = 0; i < mycurrentNum; i++) {
+            if (i < chosen - 1) {
+                temp[i] = hand[i];
+            } else if (i > chosen - 1) {
+                temp[i - 1] = hand[i];
+            }
+        }
+        mycurrentNum--;
+        hand = temp;
+    }
+
+    public void printHand() {
+        for (int i = 0; i < hand.length; i++) {
+            System.out.print(hand[i] + " ");
+        }
+    }
+
+    public Card choseCardPc(Card a) {
+        for (int i = 0; i < hand.length; i++) {
+            if (a.getRank() == hand[i].getRank()) {
+                return hand[i];
+            } else if (hand[i].getRank() == "J") {
+                return hand[i];
+
+            }
+        }
+        return hand[0];
+    }
+
+    public void removeCardFromPcHand(Card a) {
+        Card temp2[] = new Card[mycurrentNum-1];
+        for (int k = 0; k < hand.length; k++) {
+            if (hand[k] == a) {
+                for (int i = 0; i < mycurrentNum; i++) {
+                    if (i < k) {
+                        temp2[i] = hand[i];
+                    } else if (i > k) {
+                        temp2[i - 1] = hand[i];
+                    }
+
+                }
+            }
+
+        }
+        mycurrentNum--;
+        hand = temp2;
+    }
+
+}
