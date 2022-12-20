bf61abb3c9a8eb9ca38c79da30f15a61b8f49c56
Sueda Akturk <aakturk.sueda@gmail.com>
2022-12-20 19:34:19 +0300

    Card class added
    new Card class added

---
+++ Card.java
@@ -0,0 +1,18 @@
+public class Card {
+   private String Suit;
+   private String Rank;
+
+    public Card(String Suit, String Rank){
+        this.Suit=Suit;
+        this.Rank=Rank;
+    }
+
+
+    public String toString() {
+        return this.Suit.toString() + this.Rank.toString();
+    }
+
+    public String getRank() {
+        return this.Rank;
+    }
+}
