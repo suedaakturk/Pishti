020f79ec7aceae1f37980c37f4a26322ba19f0d4
Sueda Akturk <aakturk.sueda@gmail.com>
2022-12-20 19:41:26 +0300

    Main class added

---
+++ GamePlaying.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class GamePlaying {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        Deck deck= new Deck();
+        Board gameboard= new Board();
+        deck.shuffleDeck();
+        deck.cutDeck();
+        //deck.printDeck();
+        Hand playerhand= new Hand("sueda");
+        Hand cp=new Hand("Computer");
+        playerhand.addCard(deck.returnCard());
+        playerhand.addCard(deck.returnCard());
+        playerhand.addCard(deck.returnCard());
+        playerhand.addCard(deck.returnCard());
+        gameboard.addCardToBoard(deck.returnCard());
+        gameboard.addCardToBoard(deck.returnCard());
+        gameboard.addCardToBoard(deck.returnCard());
+        gameboard.addCardToBoard(deck.returnCard());
+        cp.addCard(deck.returnCard());
+        cp.addCard(deck.returnCard());
+        cp.addCard(deck.returnCard());
+        cp.addCard(deck.returnCard());
+
+        gameboard.printBoard();
+        System.out.println(" ");
+        System.out.println("----");
+        System.out.println("My hand:");
+        playerhand.printHand();
+        System.out.println(" ");
+        System.out.println("----");
+        System.out.println("pc hand:");
+        cp.printHand();
+        System.out.println(" ");
+
+        gameboard.addCardToBoard(playerhand.returnCardToBoard());
+        playerhand.removeCardFromHand();
+        gameboard.printBoard();
+        System.out.println(" ");
+        System.out.println("----");
+        gameboard.addCardToBoard(cp.choseCardPc(gameboard.returnCard()));
+        cp.removeCardFromPcHand(gameboard.returnCard());
+        cp.printHand();
+        System.out.println(" ");
+
+
+
+        System.out.println("board new");
+        gameboard.printBoard();
+        System.out.println(" ");
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
